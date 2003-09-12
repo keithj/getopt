ea0e46a74a4ed3631d4e6757e34509352f29a358
Kevin M. Rosenberg <kevin@rosenberg.net>
2003-09-12 17:08:54 +0000

    r7831: fix upload script

--- debian/upload.sh
+++ debian/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-dup getopt -Uftp.med-info.com -D/home/ftp/getopt  -C"(cd /opt/apache/htdocs/getopt; make install)" -su $*
+dup getopt -Uftp.med-info.com -D/home/ftp/getopt  -C"/home/kevin/bin/remove-old-versions getopt latest" -su $*
 
 
 
